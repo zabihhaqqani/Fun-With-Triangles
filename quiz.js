26ae4ecb65933f4f66acdcd4ef142cdf76b3aa00
zabihhaqqani <mdzabihhaqqani@gmail.com>
2022-05-21 22:09:22 +0530

    added more questions

--- quiz.js
+++ quiz.js
@@ -2,7 +2,7 @@ let form = document.querySelector(".quiz-form")
 let btn = document.querySelector(".submit-btn")
 let outputMsg = document.querySelector(".output-msg")
 
-const correctAnswers = ['90°', 'right angled'];
+const correctAnswers = ['90°', 'right angled', 'Equilateral', '√3/4 a2', '4.33cm'];
 
 function calculateScore(e) {
     e.preventDefault(e);
@@ -17,7 +17,7 @@ function calculateScore(e) {
             index++
 
     }
-    outputMsg.innerText = `Your score is ${score}`
+    outputMsg.innerText = `Your score is ${score}/5`
 }
 
 btn.addEventListener("click", calculateScore)
