d15fb55c663588b362c21f257de9c0470ec6e964
zabihhaqqani <mdzabihhaqqani@gmail.com>
2022-09-21 15:37:42 +0530

    refactor

--- area.js
+++ area.js
@@ -1,23 +1,18 @@
-let inputs = document.querySelectorAll(".input")
-let calculateBtn = document.querySelector("#calculate-area")
-let outputMsg = document.querySelector("#output-msg")
-
+let inputs = document.querySelectorAll(".input");
+let calculateBtn = document.querySelector("#calculate-area");
+let outputMsg = document.querySelector("#output-msg");
 
 function calculateAreaOfTriangle() {
+  let base = Number(inputs[0].value);
+  let height = Number(inputs[1].value);
 
-    let base = Number(inputs[0].value)
-    let height = Number(inputs[1].value)
+  let area = (base * height) / 2;
 
-    let area = (base * height)/2
-
-    if(Number(inputs[0].value)<1||Number(inputs[1].value)<1){
-        outputMsg.innerText = "please don't enter any negative values!!!!"
-    }else{
-    outputMsg.innerText = `the area is ${area} cm²`
-
-    }
-
+  if (Number(inputs[0].value) < 1 || Number(inputs[1].value) < 1) {
+    outputMsg.innerText = "please enter proper values!!!!";
+  } else {
+    outputMsg.innerText = `the area is ${area} cm²`;
+  }
 }
 
-
-calculateBtn.addEventListener("click", calculateAreaOfTriangle)
+calculateBtn.addEventListener("click", calculateAreaOfTriangle);
--- hypo.js
+++ hypo.js
@@ -1,23 +1,19 @@
-let inputs = document.querySelectorAll(".input")
-let calculateBtn = document.querySelector("#calculate-hypotenuse")
-let message = document.querySelector("#output-msg")
+let inputs = document.querySelectorAll(".input");
+let calculateBtn = document.querySelector("#calculate-hypotenuse");
+let message = document.querySelector("#output-msg");
 
 function calculateHypotenuse() {
+  let a = Number(inputs[0].value);
+  let b = Number(inputs[1].value);
 
-    let a = Number(inputs[0].value)
-    let b = Number(inputs[1].value)
+  let sum = a * a + b * b;
+  let hypotenuse = Math.sqrt(sum);
 
-    let sum = a * a + b * b
-    let hypotenuse = Math.sqrt(sum)
-
- if(Number(inputs[0].value)<1||Number(inputs[1].value)<1){
-        message.innerText = "please don't enter any negative values!!!!"
-    }else{
-    message.innerText = `The hypotenuse is ${hypotenuse} cm`
-    }
-
-
+  if (Number(inputs[0].value) < 1 || Number(inputs[1].value) < 1) {
+    message.innerText = "please enter proper values!!!!";
+  } else {
+    message.innerText = `The hypotenuse is ${hypotenuse} cm`;
+  }
 }
 
-
-calculateBtn.addEventListener("click", calculateHypotenuse)
+calculateBtn.addEventListener("click", calculateHypotenuse);
